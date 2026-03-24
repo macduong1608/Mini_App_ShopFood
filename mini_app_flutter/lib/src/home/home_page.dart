875fc2e2aa723dd2f8c985fed6d860feb6346b8b
Mac Duong <macphamhoangduong@gmail.com>
2026-03-24 13:35:50 +0700

    UI Welcome screen

---
+++ mini_app_flutter/lib/src/home/home_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:mini_app_flutter/src/widgets/button_custom.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF8F8F8),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                const Text(
+                  "SD",
+                  style: TextStyle(
+                    color: Colors.orange,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(25),
+                        child: Image.asset(
+                          'assets/home_screen.png',
+                          width: double.infinity,
+                          height: 350,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      RichText(
+                        textAlign: TextAlign.center,
+                        text: const TextSpan(
+                          style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF1C1B1B),
+                          ),
+                          children: [
+                            TextSpan(text: "SD Store: "),
+                            TextSpan(
+                              text: "Your Style\nRedefined",
+                              style: TextStyle(
+                                color: Color(0xFFB45309),
+                                fontStyle: FontStyle.italic,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25),
+                        child: const Text(
+                          'Welcome to SD Store! Let’s find the perfect outfits to make you shine every single day.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xFF7B675E),
+                            height: 1.5,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      ButtonCustom(
+                        name: 'Sign In',
+                        onTap: () {},
+                        color: 0xFFFF6600,
+                      ),
+                      const SizedBox(height: 15),
+                      ButtonCustom(
+                        name: 'Sign Up',
+                        onTap: () {},
+                        color: 0xFFFF6600,
+                        isOutlined: true,
+                      ),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
