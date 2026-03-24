3fd3b655ea842a5865d975820b7dd9355eb28b1d
macduong1608 <55268337+macduong1608@users.noreply.github.com>
2026-03-24 14:36:43 +0700

    UI login page (#2)

---
+++ mini_app_flutter/lib/src/page/login_page.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/button_custom.dart';
+import '../widgets/input_filed_custom.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF8F8F8),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                const Text(
+                  "SD",
+                  style: TextStyle(
+                    color: Colors.orange,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFFFFFF),
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(color: Color(0xFFFFFFFF), width: 0.5),
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Sign in to SD Store',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF1C1B1B),
+                        ),
+                      ),
+                      Text(
+                        'Please enter your details to sign in',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                          color: Color(0xFF8B7A72),
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(25),
+                        child: Image.asset(
+                          'assets/login_screen.png',
+                          width: double.infinity,
+                          height: 180,
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      SizedBox(
+                        width: double.infinity,
+                        child: Column(
+                          children: [
+                            inputFiledCustom(
+                              "Email or Phone",
+                              "Enter your email",
+                            ),
+                            inputFiledCustom(
+                              "Password",
+                              "Enter your password",
+                              isPassword: true,
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: TextButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  "Forgot password?",
+                                  style: TextStyle(
+                                    color: Color(0xFFFF6600),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      ButtonCustom(
+                        name: 'Sign In',
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const LoginPage(),
+                            ),
+                          );
+                        },
+                        color: 0xFFFF6600,
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Don't have an account?",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                              color: Color(0xFF9C8D86),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "Register",
+                              style: TextStyle(
+                                color: Color(0xFFFF6600),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
